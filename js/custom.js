db1ec1720d3aba008c1cb96bf42b834ee23a51df
Yash Patel <yash261261@gmail.com>
2020-06-16 13:10:47 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -33,9 +33,8 @@ $(document).ready(function() {
   $(function() {
     $('body').vegas({
         slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
+            { src: 'images/yash1.jpg' },
+            { src: 'images/yash2.jpg' },
         ],
         timer: false,
         transition: [ 'zoomIn', ],
