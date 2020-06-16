0f3abc774db0644ef31ac53e9edbe365ecb1172c
Yash Patel <yash261261@gmail.com>
2020-06-16 18:42:12 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="yash261261@gmail.com";
+    $subject="Website Email Message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
